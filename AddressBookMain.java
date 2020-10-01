099b60d50f154b8d1ea534ddefba716965ccd570
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-01 18:04:33 +0530

    [deekshita] Add : Added usecase6

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,15 +2,24 @@ import java.util.*;
 public class AddressBookMain
 {
       private ArrayList<PersonDetails> detailsArrayList;
+      private Map<String, PersonDetails>detailsMap;
       private AddressBookMain()
       {
               detailsArrayList = new ArrayList<>();
+              detailsMap = new HashMap<>();
       }
       private void addPersonDetails(String firstName, String lastName, String address, String city, String state, int zip, String phoneNum, String emailId)
       {
               PersonDetails pd = new PersonDetails();
               pd.setPersonDetails(firstName, lastName, address, city, state, zip, phoneNum, emailId);
               detailsArrayList.add(pd);
+              detailsMap.put(firstName.concat(" "+lastName),pd);
+      }
+
+      private void viewPersonDetails(String name)
+      {
+               PersonDetails pdObj= detailsMap.get(name);
+               System.out.println(pdObj);
       }
 
 /*      private void editPersonDetails(String firstName, String lastName, String address, String city, String state, int zip, String phoneNum, String emailId)
@@ -18,12 +27,12 @@ public class AddressBookMain
                 int flag = 0;
                 for(int i = 0; i < noOfPersons; i++)
                 {
-			if(detailsArray[i].firstName.equals(firstName) && detailsArray[i].lastName.equals(lastName))
+			if(detailsArrayList[i].firstName.equals(firstName) && detailsArrayList[i].lastName.equals(lastName))
                         {
                                 flag=1;
-				detailsArray[i].setPersonDetails(firstName, lastName, address, city, state, zip, phoneNum, emailId);
+				detailsArrayList[i].setPersonDetails(firstName, lastName, address, city, state, zip, phoneNum, emailId);
 				System.out.println("Details Updated");
-				System.out.println(detailsArray[i]);
+				System.out.println(detailsArrayList[i]);
 				break;
 			}
 		}
@@ -38,10 +47,10 @@ public class AddressBookMain
                int flag = 0;
                for(int i= 0; i < noOfPersons ; i++)
                {
-                        if(detailsArray[i].firstName.equals(firstN) && detailsArray[i].lastName.equals(lastN))
+                        if(detailsArrayList[i].firstName.equals(firstN) && detailsArrayList[i].lastName.equals(lastN))
                         {
                               flag = 1;
-                              detailsArray[i]=null;
+                              detailsArrayList[i]=null;
                               System.out.println("Details deleted");
                               break;
                         }
@@ -51,8 +60,8 @@ public class AddressBookMain
                         System.out.println("No such name in records");
                }
      }
-*/
-      public static void main(String[] args)
+
+  */    public static void main(String[] args)
       {
 		Scanner sc = new Scanner(System.in);
 		AddressBookMain ad = new AddressBookMain();
@@ -89,7 +98,7 @@ public class AddressBookMain
                 switch(option)
                 {
                         case 1:
-*/	/*	        System.out.println("Enter the name of the contact you want to edit and details to be modified");
+*//*		        System.out.println("Enter the name of the contact you want to edit and details to be modified");
 			System.out.println("First Name: ");
 			String firstName = sc.nextLine();
                         sc.nextLine();
@@ -109,17 +118,22 @@ public class AddressBookMain
 			System.out.println("Email ID: ");
 			String emailId = sc.nextLine();
                         ad.editPersonDetails(firstName,lastName,address,city,state,zip,phoneNum,emailId);
- */ //                       break;
+ //                       break;
   //                      case 2:
-    /*                    System.out.println("Enter the name of the person whose details are to be deleted");
+                        System.out.println("Enter the name of the person whose details are to be deleted");
                         System.out.println("Enter the first name :");
                         String firstN = sc.nextLine();
                         sc.nextLine();
                         System.out.println("Enter the last name :");
                         String lastN = sc.nextLine();
                         ad.deletePersonDetails(firstN,lastN);
-*/                        /*break;
-                        default:
+                        /*break;
+                        case 3:
+*/                        System.out.println("Enter first and last name of person with space whom you want to print details : ");
+		        String name = sc.nextLine();
+		        ad.viewPersonDetails(name);
+
+/*                        default:
                         System.out.println("Wrong option");
                 }*/
 
